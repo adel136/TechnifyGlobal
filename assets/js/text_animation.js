f6070197a110929a2dc1ceabc77e262debcd7b7b
Adel Rabea <a.rabea@infoglobe.eg>
2024-07-16 18:14:43 +0300

    create project

---
+++ assets/js/text_animation.js
@@ -0,0 +1,15 @@
+
+
+	function tm_animate_text(){
+	
+		"use strict";
+		
+		var animateSpan			= jQuery('.animation_text_word');
+
+	}
+
+	jQuery(document).on('ready', function () {
+		(function ($) {
+			tm_animate_text();
+		})(jQuery);
+	});
